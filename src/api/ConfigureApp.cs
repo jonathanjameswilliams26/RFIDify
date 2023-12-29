e810d233bd6962dce0dd69f5ce108b9310347d18
Jonathan Williams <jonathanjameswilliams26@gmail.com>
2023-12-29 20:40:57 +1100

    Reorganised the default template

---
+++ src/api/ConfigureApp.cs
@@ -0,0 +1,14 @@
+﻿namespace RFIDify;
+
+public static class ConfigureApp
+{
+	/// <summary>
+	/// Configures the HTTP request pipeline.
+	/// </summary>
+	public static void Configure(this WebApplication app)
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+		app.UseHttpsRedirection();
+	}
+}
---
+++ src/api/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿namespace RFIDify;
+
+public static class ConfigureServices
+{
+	/// <summary>
+	/// Configures the application services.
+	/// </summary>
+	public static void AddServices(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+	}
+}
